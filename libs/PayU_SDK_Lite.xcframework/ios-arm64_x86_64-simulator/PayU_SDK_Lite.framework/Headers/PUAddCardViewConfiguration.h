04afbda66ecc5ac541cfbd7f0f5b809fdf3d6554
Anton Ivashyna <anton.ivashyna@payu.com>
2022-10-14 09:29:35 +0200

    Update source code. Update documentation. Update s.version to 1.0.12.

---
+++ libs/PayU_SDK_Lite.xcframework/ios-arm64_x86_64-simulator/PayU_SDK_Lite.framework/Headers/PUAddCardViewConfiguration.h
@@ -0,0 +1,33 @@
+//
+// Copyright © 2021 PayU SA. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+#import "PUCardRecognizerOption.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PUAddCardViewConfiguration : NSObject
+@property (assign, nonatomic) BOOL isHeaderVisible;
+@property (assign, nonatomic) BOOL isHeaderCardProviderVisible;
+@property (assign, nonatomic) BOOL isTextInputLabelsVisible;
+@property (assign, nonatomic) BOOL isTextInputCardProviderVisible;
+@property (assign, nonatomic) BOOL isTextInputCardScannerVisible;
+@property (assign, nonatomic) BOOL isTextInputCVVActionVisible;
+@property (assign, nonatomic) BOOL isFooterVisible;
+
+@property (strong, nonatomic) NSString *cardNumberLabel;
+@property (strong, nonatomic) NSString *cardNumberPlaceholder;
+
+@property (strong, nonatomic) NSString *cardDateLabel;
+@property (strong, nonatomic) NSString *cardDatePlaceholder;
+
+@property (strong, nonatomic) NSString *cardCVVLabel;
+@property (strong, nonatomic) NSString *cardCVVPlaceholder;
+@property (strong, nonatomic) NSString *cardCVVAction;
+
+@property (assign, nonatomic) PUCardRecognizerOption option;
+
+@end
+
+NS_ASSUME_NONNULL_END
