070c9558bf34a57d7ab7e138bfd07192d7865c59
jakub-wrzesniak-payu-gpo <jakub.wrzesniak@payu.com>
2026-01-30 10:27:51 +0100

    Add Visa card number length validation before switch to the next field.

--- PUPaymentCard/Sources/PUPaymentCard/Widget/PaymentCardTextInputView.swift
+++ PUPaymentCard/Sources/PUPaymentCard/Widget/PaymentCardTextInputView.swift
@@ -1,6 +1,6 @@
 //
 //  PaymentCardTextInputView.swift
-//  
+//
 //  Created by PayU S.A. on 12/12/2022.
 //  Copyright © 2022 PayU S.A. All rights reserved.
 //
@@ -95,11 +95,13 @@ extension PaymentCardTextInputView: UITextFieldDelegate {
 
     textField.text = formattedText
     onTextDidChange?(formattedText)
+    
 
-    if !editedText.isEmpty && (nextActiveResponder != nil) {
+    if (nextActiveResponder != nil) {
         do {
-          try validator.validate(editedText)
-          nextActiveResponder?.becomeFirstResponder()
+            if (try validator.canBeCompleted(editedText)){
+                nextActiveResponder?.becomeFirstResponder()
+            }
         } catch {
         }
     }
